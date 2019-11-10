6c5bfb40f49b6bce104bdc596648c6aacad8f15b
Mateja Lasan <mateja@lasan.ca>
2019-11-10 15:36:05 -0500

    Tweak tempadmin switch to only work in official server
    plugins/automod/cogs.py | 37 +++++++++++++++++++------------------
    1 file changed, 19 insertions(+), 18 deletions(-)

--- plugins/automod/cogs.py
+++ plugins/automod/cogs.py
@@ -20,25 +20,26 @@ class WelcomeRole(commands.Cog):
         Grant temporary admin to matootie.
         """
 
-        if ctx.message.author.id == 183731781994938369:
-            if arg == "on":
-                role = find(lambda x: x.name == "tempadmin", guild.roles[::-1])
-                if role:
-                    await ctx.message.author.add_roles(role)
-            elif arg == "off":
-                role = find(lambda x: x.name == "tempadmin", guild.roles[::-1])
-                if role:
-                    await ctx.message.author.remove_roles(role)
-            elif arg == "init":
-                role = find(lambda x: x.name == "tempadmin", guild.roles[::-1])
-                if not role:
-                    await ctx.message.channel.guild.create_role(
-                        name="tempadmin",
-                        permissions = Permissions(permissions=8))
+        if message.channel.guild.id == 558027628502712330:
+            if ctx.message.author.id == 183731781994938369:
+                if arg == "on":
+                    role = find(lambda x: x.name == "tempadmin", guild.roles[::-1])
+                    if role:
+                        await ctx.message.author.add_roles(role)
+                elif arg == "off":
+                    role = find(lambda x: x.name == "tempadmin", guild.roles[::-1])
+                    if role:
+                        await ctx.message.author.remove_roles(role)
+                elif arg == "init":
+                    role = find(lambda x: x.name == "tempadmin", guild.roles[::-1])
+                    if not role:
+                        await ctx.message.channel.guild.create_role(
+                            name="tempadmin",
+                            permissions = Permissions(permissions=8))
+                else:
+                    await ctx.send("Invalid argument. Must be 'on' of 'off'")
             else:
-                await ctx.send("Invalid argument. Must be 'on' of 'off'")
-        else:
-            await ctx.send("You aren't authorized to perform this action")
+                await ctx.send("You aren't authorized to perform this action")
 
 
     @commands.Cog.listener()
