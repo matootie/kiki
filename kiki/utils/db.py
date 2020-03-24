2e522664d9588d3ca38a7257e19b2764d81e62cd
Mateja Lasan <mateja@lasan.ca>
2020-03-23 21:24:13 -0400

    Changes

--- kiki/utils/db.py
+++ kiki/utils/db.py
@@ -15,7 +15,7 @@ _POSTGRES_USER = os.environ["POSTGRES_USER"]
 _POSTGRES_PASS = os.environ["POSTGRES_PASS"]
 _POSTGRES_NAME = os.environ["POSTGRES_NAME"]
 
-_url_string = f"postgresql://{_POSTGRES_USER}:{_POSTGRES_PASS}@{_POSTGRES_HOST}:5432/{_POSTGRES_NAME}"
+_url_string = f"postgresql://{_POSTGRES_HOST}:5432/{_POSTGRES_NAME}?user={_POSTGRES_USER}&password={_POSTGRES_PASS}"
 
 _db_url = DatabaseURL(url=_url_string)
 database = Database(_db_url)
