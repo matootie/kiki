647390a2781a3f55883214f7e52f1f0af487927d
Mateja Lasan <mateja@lasan.ca>
2020-04-01 00:18:24 -0400

    Strip newline from database password

--- kiki/utils/db.py
+++ kiki/utils/db.py
@@ -10,12 +10,12 @@ from sqlalchemy import MetaData
 
 # Establish an instance of the database, and SQLAlchemy metadata
 # for use with the ORM.
-_POSTGRES_HOST = os.environ["POSTGRES_HOST"]
-_POSTGRES_USER = os.environ["POSTGRES_USER"]
-_POSTGRES_PASS = os.environ["POSTGRES_PASS"]
-_POSTGRES_NAME = os.environ["POSTGRES_NAME"]
+_PG_H = os.environ["POSTGRES_HOST"]
+_PG_U = os.environ["POSTGRES_USER"]
+_PG_P = os.environ["POSTGRES_PASS"].rstrip("\n")
+_PG_N = os.environ["POSTGRES_NAME"]
 
-_url_string = f"postgresql://{_POSTGRES_USER}:{_POSTGRES_PASS}@{_POSTGRES_HOST}:5432/{_POSTGRES_NAME}"
+_url_string = f"postgresql://{_PG_H}:{_PG_P}@{_PG_H}:5432/{_PG_N}"
 
 _db_url = DatabaseURL(url=_url_string)
 database = Database(_db_url)
